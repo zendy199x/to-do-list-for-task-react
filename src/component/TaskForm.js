276a96e2619ada700406c9e3e930a1d6863d0a52
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-11-07 15:17:45 +0700

    'add'

--- src/component/TaskForm.js
+++ src/component/TaskForm.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class TaskForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            name: "",
+            status: false
+        })
+    }
 
     onCloseForm = () => {
         this.props.onCloseForm();
+    }
+
+    onChange = (event) => {
+        const target =  event.target;
+        const name = target.name;
+        let value = target.value
+        if(name === "status") {
+            value = target.value === "true" ? true : false;
+        }
+        this.setState({
+            [name] : value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+        this.onClear();
+        this.onCloseForm();
+    }
+
+    onClear = () => {
+        this.setState({
+            name: "",
+            status: false
+        })
     }
 
     render() {
@@ -27,6 +60,8 @@ class TaskForm extends Component {
                                     type="text" 
                                     className="form-control"
                                     name="name"
+                                    value={this.state.name}
+                                    onChange={this.onChange}
                                 />
                             </div>
                             <label>Trạng Thái :</label>
@@ -34,18 +69,26 @@ class TaskForm extends Component {
                                 className="form-control" 
                                 required="required"
                                 name="status"
+                                value={this.state.status}
+                                onChange={this.onChange}
                             >
                                 <option value={true}>Kích Hoạt</option>
                                 <option value={false}>Ẩn</option>
                             </select>
                             <br />
                             <div className="text-center">
-                                <button type="submit" className="btn btn-warning">Thêm</button>&nbsp;
                                 <button 
-                                    type="submit" 
+                                    type="submit" className="btn btn-warning">
+                                    <span className="fa fa-plus mr-5"></span>
+                                    Lưu lại
+                                </button>&nbsp;
+                                <button 
+                                    type="button" 
                                     className="btn btn-danger"
+                                    onClick={this.onClear}
                                 >
-                                    Hủy Bỏ
+                                    <span className="fa fa-plus mr-5"></span>
+                                    Hủy bỏ
                                 </button>
                             </div>
                         </form>
